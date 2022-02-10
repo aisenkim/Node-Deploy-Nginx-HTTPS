06402f6ba8761f629e0cc9c3d1d48d4bdda54bb9
Aisen Kim <chunghkim@cs.stonybrook.edu>
2022-02-09 20:20:14 -0500

    edited /ttt/play post route

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.use(express.urlencoded({extended : true}))
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req,res) => {
-    res.send("Hello World")
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-app.post('/', (req,res) => {
-    console.log(req.body.name)
-    const date = new Date()
-    res.send(`Hello ${req.body.name}, ${date}`)
-})
+app.post("/", (req, res) => {
+  const date = new Date();
+  res.send(`Hello ${req.body.name}, ${date}`);
+});
+
+app.post("/ttt/play", (req, res) => {
+  res.send("Sent from the server /ttt/play");
+});
 
 app.listen(port, () => {
-    console.log(`Example App For Deploying Nginx SSL on port: ${port}`)
-})
-
+  console.log(`Example App For Deploying Nginx SSL on port: ${port}`);
+});
