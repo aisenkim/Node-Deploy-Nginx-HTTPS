b93ab041015aa7365e8547314f26bbaca39a626e
Aisen Kim <chunghkim@cs.stonybrook.edu>
2022-02-09 19:57:18 -0500

    edited index file

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.urlencoded({extended : true}))
+
 app.get('/', (req,res) => {
     res.send("Hello World")
+})
+
+app.post('/', (req,res) => {
+    console.log(req.body.name)
+    const date = new Date()
+    res.send(`Hello ${req.body.name}, ${date}`)
 })
 
 app.listen(port, () => {
